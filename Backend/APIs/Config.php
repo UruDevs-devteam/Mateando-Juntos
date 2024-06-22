c5a3128aa19e77b6d706bd19686eb17d1703eb17
eze <ezequieldurante640@gmail.com>
2024-06-22 16:22:14 -0300

    APi. version 1

---
+++ Backend/APIs/Config.php
@@ -0,0 +1,8 @@
+<?php
+$Nombre_Host = 'localhost';
+$Nombre_Usuario = 'root';
+$contra = 'mysqlezequiel1_';
+$Basde_datos = 'pmate';
+
+$conex = mysqli_connect($Nombre_Host, $Nombre_Usuario, $contra, $Basde_datos);
+
